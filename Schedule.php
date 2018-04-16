8cb4a558aba21cf228ee2e00014058a00336decf
LoulaAg <loula.ag.13@gmail.com>
2018-04-16 14:16:25 -0600

    Add files via upload

--- Schedule.php
+++ Schedule.php
@@ -3,33 +3,24 @@ session_start();
 $db_username = "root";
 $db_password = "";
 $server_name = "127.0.0.1";
-$db_name = "testworkout";
+$db_name = "workout";
 $username = $_SESSION['user'];
 $workout = $_POST['Workout'];
 $day = $_POST['password'];
 
 
-if(isset($_POST['register'])) {
+if(isset($_POST['submit'])) {
     try {
         $conn = new PDO("mysql:host=$server_name;dbname=$db_name", $db_username, $db_password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //echo "Connected successfully";
-        $sql = "INSERT INTO Scheduled_on (Esername, Workout_Name, Day) VALUES ('$username', '$wokrout', '$day')";
+        $sql = "INSERT INTO scheduled_on (Username, Workout_Name, Day) VALUES ('$username', '$workout', '$day')";
         $conn->exec($sql);
-        $_SESSION['user'] = $username;
         
     } catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
     }
-}
-
-if(isset($_SESSION['user'])){
-    header('Location: Profile.html');
-    exit;
-} else {
-    header('Location: Login.html');
-    exit;
 }
 
 ?>
